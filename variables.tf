d12cfde08a8d1985c5759abe42e04558f18a6943
Sandeep-Madhiraju <87055139+Sandeep-Madhiraju@users.noreply.github.com>
2021-09-09 22:06:08 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,7 +12,7 @@ variable "prefix" {
 
 variable "region" {
   description = "The region where the resources are created."
-  default     = "us-east-1"
+  default     = "ap-south-1"
 }
 
 variable "address_space" {
