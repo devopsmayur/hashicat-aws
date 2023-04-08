9445714a179159163e6276a3ec9f6926b297445d
Mayur <99334268+devopsmayur@users.noreply.github.com>
2023-04-08 21:37:43 +0530

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,5 @@ module "s3-bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "2.8.0"
   # insert required variables here
-  bucket_prefix = mayur
+  bucket_prefix = "mayur"
 }
